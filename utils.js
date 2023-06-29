e6a5db0452b1f2af33d9dbd33ececec0280851f3
Billy Rennekamp <billy.rennekamp@gmail.com>
2023-06-29 15:25:21 +0200

    update viper with bite

--- utils.js
+++ utils.js
@@ -4,8 +4,14 @@ function extractBiteId(tokenId) {
   tokenId = ethers.BigNumber.from(tokenId)
   // length is tokenId bit shifted right 169 bits
   const length = tokenId.shr(169)
+  if (length.lt(1)) {
+    throw new Error(`Invalid length ${length} for tokenId ${tokenId}`)
+  }
   // originalTokenId is tokenId bit shifted right 160 bits and then masked with 0x1ff
   const originalTokenId = tokenId.shr(160).and(0x1ff)
+  if (originalTokenId.lt(1) || originalTokenId.gt(486)) {
+    throw new Error(`Invalid originalTokenId ${originalTokenId} for tokenId ${tokenId}`)
+  }
   // senderAddress is tokenId masked with 0xffffffffffffffffffffffffffffffffffffffff
   const senderAddress = tokenId.and("0xffffffffffffffffffffffffffffffffffffffff")
   return { length, originalTokenId, senderAddress }
@@ -21,5 +27,14 @@ var refreshOpensea = function (network, address, tokenID) {
     .catch(error => { console.log(error) })
 }
 
+async function reverseLookup(address) {
+  const provider = new ethers.providers.InfuraProvider(
+    "homestead",
+    process.env.INFURA_API_KEY,
+  );
+  const name = await provider.lookupAddress(address)
+  return name || address
+}
+
 // export extractBiteId
-module.exports = { extractBiteId, refreshOpensea }
+module.exports = { extractBiteId, refreshOpensea, reverseLookup }
